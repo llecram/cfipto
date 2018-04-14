f07a16eb82b567e3c658e80e41ee62423cb65973
Dennis <31170537+llecram@users.noreply.github.com>
2018-04-14 13:38:42 -0500

    Update iz.cpp

--- iz.cpp
+++ iz.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 string alfabeto="abcdefghijklmnopqrstuvwxyz_";
 string volta(string b){
-    
     string temp=b;
     for(int i=0;i<b.size();i++){
         for(int j=0;j<alfabeto.size();j++){
@@ -21,14 +20,31 @@ string cif(string a,int clave){
     }
     for(int i=0;i<clave;i++){
         for(int j=0;j<a.size();j+=elementosola){
-            
+
         }
     }
+}
+string rellenarx(string a,int clave){
+    string aux=a;
+    for(int i=0;i<100000;i+=(clave-1)){
+        if(i>(a.size()-1)){
+            //cout<<endl<<i<<endl;
+            for(int j=(a.size()-1);j<i;j++){
+                aux+='x';
+            }
+            break;
+        }
+    }
+    return aux;
 }
 int main(){
     string mensaje;
     cout<<"ingrese el mensaje: ";
     cin>>mensaje;
     string result=volta(mensaje);
+    cout<<endl<<endl<<result;
     cout<<endl<<result;
+    string aux=rellenarx(mensaje,5);
+    cout<<endl<<endl<<aux;
+
 }
